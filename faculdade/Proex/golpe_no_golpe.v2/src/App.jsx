5eae8959483a38be059ea50a2c3624eec68008dd
Henrique Bicudo <henrique.luiz.bicudo@gmail.com>
2025-11-02 22:06:14 -0300

    feat: redesign para tela preta com animação de carregamento

---
+++ faculdade/Proex/golpe_no_golpe.v2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="content">
+        <h1 className="title">Site em Construção</h1>
+        <div className="loader">
+          <div className="spinner"></div>
+        </div>
+        <p className="message">Carregando...</p>
+      </div>
+    </div>
+  )
+}
